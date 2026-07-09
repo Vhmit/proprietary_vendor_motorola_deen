7a98f4b614edca7806e99a1c692058ad5d3939c8
Vhmit <viktor.9630@protonmail.com>
2026-07-09 20:56:04 -0300

    deen: Kang GPS stack from river_retail:10/QPUS30.52-33-11
    Signed-off-by: Vhmit <viktor.9630@protonmail.com>

--- deen-vendor.mk
+++ deen-vendor.mk
@@ -996,4 +996,5 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.factory-V1.1-java \
     vendor.qti.ims.callinfo-V1.0-java \
     vendor.qti.ims.rcsconfig-V1.0-java \
-    vendor.qti.latency-V2.0-java
+    vendor.qti.latency-V2.0-java \
+    vendor.qti.gnss@3.0-service
